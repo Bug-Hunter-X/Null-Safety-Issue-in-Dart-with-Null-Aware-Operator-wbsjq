85faf1ccdd305eaabe432e22e6535603bf21e5fa
BugHunter-X <askzenai@gmail.com>
2025-02-04 13:08:05 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,12 @@
+```dart
+class MyClass {
+  int? _myVariable;
+
+  MyClass(this._myVariable);
+
+  void myMethod() {
+    print(_myVariable ?? 0); // Potential error if _myVariable is null
+    // Other code here
+  }
+}
+```
